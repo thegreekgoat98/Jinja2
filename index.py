cdda182ecacd68654f5678eba9056d77c429305f
Chinmoy Ranjan Khadgaray <ranjanchinmoy@gmail.com>
2023-06-29 12:15:51 +0530

    added anothe html page

--- index.py
+++ index.py
@@ -5,7 +5,16 @@ app=FastAPI()
 
 templates=Jinja2Templates(directory="templates")
 
+BATSMAN = [{"name":"Sachin Tendulkar", "side":"Right"},{"name":"Brian Lara", "side":"Left"}, {"name":"Virat Kohli","side":"Right"}]
+
 @app.get("/")
 def home(request:Request):
     return templates.TemplateResponse("home.html",{"request":request, "name":"Hello World"})
+
+
+@app.get("/batsman")
+def show(request:Request):
+    return templates.TemplateResponse("batsman.html",{"request":request,"batsman":BATSMAN})
+
+
     
